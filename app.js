07f703096cc17760fbf60e1e9c16a2b6527b744b
Ashish Akotkar <ashish.akotkar.osv@fedex.com>
2024-12-11 11:32:57 +0100

    updated message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, DevOps POC!');
+    res.send('Hello, DevOps POC!!');
 });
 
 app.listen(3000, () => {
